eb3417c1ad29d2763559b2156dc98da85489f87b
Reda-R <mohamed-reda.ramdani@epitech.eu>
2022-12-29 16:25:39 +0100

    use of argparse and optimisation for config script (port set by default to the bootstrap list) and update/remove useless statments

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 import sys
 import shutil
 from pathlib import Path
+import argparse
 
 # Third Part Library
 import tomli
 import tomli_w
 
 class Config():
-    def __init__(self, argv):
+    def __init__(self, argv, args_list):
         self.len_args = len(argv)
         self.args = argv
-        self.config_file = self.args[1]
-        self.ip = self.args[2]
-        self.address = self.args[3]
+        self.config_file = args_list[0]
+        self.ip = args_list[1]
+        self.address = args_list[2]
         self.toml_dict = {}
         self.bs_list_section_dict = {}
         self.list_bs_node = []
@@ -24,13 +25,16 @@ class Config():
         configfile_path = Path("/", "home", "user", "massa_exec_files", "massa-node", "base_config", "config.toml")
         if configfile_path.exists() == False:
             print("ERROR : File '" + str(configfile_path) + "' not found", file=sys.stderr)
-            sys.exit(-1)
+            sys.exit(2)
 
 
     def fill_bs_list(self):
         for i in range(2, self.len_args):
             if self.args[i]:
-                self.list_bs_node.append(self.args[i])
+                if i == 2:
+                    self.list_bs_node.append(self.args[i] + ":31245")
+                else:
+                    self.list_bs_node.append(self.args[i])
         return self.list_bs_node
 
 
@@ -58,44 +62,32 @@ class Config():
             tomli_w.dump(self.toml_dict, f)
 
 
-
-def usage(status):
-    print("USAGE :\n"
-        "\tpython config.py [file] [ip] [adress]\n"
-        "\nDESCRIPTION :\n"
-        "\tfile\t\tconfiguration file\n"
-        "\tip\t\tip of the node for the bootstrap\n"
-        "\taddress\t\twallet address"
-    )
-
-
-def main(argv):
+def main(argv, args_list):
     try:
-        if len(argv) == 2 and argv[1] == "--help":
-            usage()
-            sys.exit(0)
-        elif len(argv) != 4:
-            usage()
-            sys.exit(-1)
-        cfg = Config(argv)
+        cfg = Config(argv, args_list)
         cfg.check_file()
         cfg.get_file_content()
         cfg.change_bs_sections()
         cfg.gen_config_file()
     except FileNotFoundError:
         print("ERROR : File '" + str(argv[1]) + "' not found", file=sys.stderr)
-        sys.exit(-1)
+        sys.exit(2)
     except IOError:
         print("ERROR: IO Error", file=sys.stderr)
-        sys.exit(-1)
-    except TypeError:
-        print("ERROR: Invalid number of arguments. See the usage bellow.\n", file=sys.stderr)
-        usage()
-        sys.exit(-1)
+        sys.exit(2)
     except tomli.TOMLDecodeError:
         print("ERROR: Cannot generate a new file or invalid TOML file", file=sys.stderr)
-        sys.exit(-1)
+        sys.exit(2)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    config_file = ""
+    ip = ""
+    address = ""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="configuration file in TOML format")
+    parser.add_argument("ip", help="ip of the node for the bootstrap")
+    parser.add_argument("address", help="wallet address")
+    args = parser.parse_args()
+    args_list = [args.config_file, args.ip, args.address]
+    main(sys.argv, args_list)
