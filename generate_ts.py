b67d037ac34ab0e12d2a548c76c867b0b32b1d9a
Reda-R <mohamed-reda.ramdani@epitech.eu>
2023-01-12 10:47:23 +0100

    remove test file and get the version main of generate_ts

--- generate_ts.py
+++ generate_ts.py
@@ -10,26 +10,22 @@ class Info:
     file_content: list[str] = []
 
 
-def get_current_timestamp(genesis_timestamp_delay) -> str:
+def get_current_timestamp(genesis_timestamp_delay):
     current_date = datetime.datetime.now()
     current_ts = current_date + datetime.timedelta(seconds=genesis_timestamp_delay)
     current_ts = round(current_ts.timestamp()) * 1000
-    #return ici et formate dans get_content_file
     current_ts = '"' + str(current_ts) + '"'
     return current_ts
 
 
-def get_content_file(info, genesis_timestamp_delay) -> str:
-    genesis_ts = ""
+def get_content_file(info, genesis_timestamp_delay):
     with open(info.env_file, "r") as f:
         info.file_content = f.readlines()
         info.len_content = len(info.file_content)
         for i in range(len(info.file_content)):
             if "GENESIS_TIMESTAMP=" in info.file_content[i]:
-                genesis_ts = str(get_current_timestamp(genesis_timestamp_delay))
-                info.file_content[i] = "GENESIS_TIMESTAMP=" + genesis_ts
+                info.file_content[i] = "GENESIS_TIMESTAMP=" + get_current_timestamp(genesis_timestamp_delay)
         f.close()
-    return genesis_ts
 
 
 def gen_new_env(info):
@@ -40,11 +36,9 @@ def gen_new_env(info):
 
 
 def main(args):
+    info = Info()
     try:
-        info = Info()
-        genesis_ts = get_content_file(info, args.genesis_timestamp_delay)
-        #convert timestamp to date and print it
-        print("genesis_timestamp = " + genesis_ts)
+        get_content_file(info, args.genesis_timestamp_delay)
         shutil.copy(info.env_file, info.env_file + ".old")
         gen_new_env(info)
     except IOError:
