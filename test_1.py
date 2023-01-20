3405efba0fce602acf58bc4c2b02a27407afbd5d
Reda-R <mohamed-reda.ramdani@epitech.eu>
2023-01-20 16:13:37 +0100

    fix the archi of unit test file to make class indepente and function request generic to build our own lib + use of getattr() function

--- test_1.py
+++ test_1.py
@@ -5,8 +5,28 @@ import unittest
 import requests
 
 
+def get_env_data(env_path: str) -> dict:
+    env_dict = {}
+    with open(env_path) as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        line = line.strip()
+        if not line or line.startswith("#") or "=" not in line:
+            continue
+        if "#" in line:
+            line = line.split("#")[0].strip()
+        key, value = line.split("=", maxsplit=1)
+        if '"' in value:
+            value = value.replace('"', '')
+        env_dict[key] = value
+    return env_dict
+
+
 class JsonApi():
-    def get_status(self, url_public: str):
+    #def __init__(self) -> None:
+    #    self.headers, self.payload = self.get_status()
+
+    def get_status(self):
         headers = {'Content-type': 'application/json'}
         payload = json.dumps({
             "jsonrpc": "2.0",
@@ -14,63 +34,43 @@ class JsonApi():
             "id": 0,
             "params": []
         })
-        response = requests.post(url_public, data=payload, headers=headers)
-        return response.json()
+        return headers, payload
 
 
 class Api(JsonApi):
     def __init__(self) -> None:
-        #JsonApi.__init__(self)
-        self.env_dict = self.get_env_data(".env")
-        self.status_content_1 = self.get_status(self.env_dict["URL_PUBLIC_NODE_1"])
-        self.status_content_2 = self.get_status(self.env_dict["URL_PUBLIC_NODE_2"])
+        self.headers, self.payload = getattr(JsonApi, 'get_status')('self')
 
-    def get_env_data(self, env_path: str) -> dict:
-        env_dict = {}
-        with open(env_path) as f:
-            lines = f.read().splitlines()
-        for line in lines:
-            line = line.strip()
-            if not line or line.startswith("#") or "=" not in line:
-                continue
-            if "#" in line:
-                line = line.split("#")[0].strip()
-            key, value = line.split("=", maxsplit=1)
-            if '"' in value:
-                value = value.replace('"', '')
-            env_dict[key] = value
-        return env_dict
+    def make_request(self, url_public: str):
+        response = requests.post(url_public, data=self.payload, headers=self.headers)
+        return response.json()
 
 
 class TestStringMethods(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
+        self.env_dict = get_env_data(".env")
         self.api_data = Api()
 
     def test_status_nodes(self):
+        req_node_1_public = getattr(self.api_data, 'make_request')(self.env_dict["URL_PUBLIC_NODE_1"])
+        req_node_2_public = getattr(self.api_data, 'make_request')(self.env_dict["URL_PUBLIC_NODE_2"])
+        
         # Test node_1_ban_count
-        ban_count_1 = self.api_data.status_content_1["result"]["network_stats"]["banned_peer_count"]
-        self.assertEqual(ban_count_1, 0)
+        self.assertEqual(req_node_1_public["result"]["network_stats"]["banned_peer_count"], 0)
         # Test node_2_ban_count
-        ban_count_2 = self.api_data.status_content_2["result"]["network_stats"]["banned_peer_count"]
-        self.assertEqual(ban_count_2, 0)
+        self.assertEqual(req_node_2_public["result"]["network_stats"]["banned_peer_count"], 0)
         # Test node_1_ip
-        node_1_ip = self.api_data.status_content_1["result"]["node_ip"]
-        self.assertEqual(node_1_ip, self.api_data.env_dict["NODE_1_IP"])
+        self.assertEqual(req_node_1_public["result"]["node_ip"], self.env_dict["NODE_1_IP"])
         # Test node_2_ip
-        node_2_ip = self.api_data.status_content_2["result"]["node_ip"]
-        self.assertEqual(node_2_ip, self.api_data.env_dict["NODE_2_IP"])
+        self.assertEqual(req_node_2_public["result"]["node_ip"], self.env_dict["NODE_2_IP"])
         # Test node_1_peers_by_id
-        connected_node_1_id = self.api_data.status_content_1["result"]["connected_nodes"]
-        self.assertIn(self.api_data.status_content_2["result"]["node_id"], connected_node_1_id)
+        self.assertIn(req_node_2_public["result"]["node_id"], req_node_1_public["result"]["connected_nodes"])
         # Test node_2_peers_by_id
-        connected_node_2_id = self.api_data.status_content_2["result"]["connected_nodes"]
-        self.assertIn(self.api_data.status_content_1["result"]["node_id"], connected_node_2_id)
+        self.assertIn(req_node_1_public["result"]["node_id"], req_node_2_public["result"]["connected_nodes"])
         # Test node_1_peers_by_ip
-        connected_node_1_ip = self.api_data.status_content_1["result"]["connected_nodes"]
-        self.assertIn(self.api_data.env_dict["NODE_2_IP"], str(connected_node_1_ip))
+        self.assertIn(self.env_dict["NODE_2_IP"], str(req_node_1_public["result"]["connected_nodes"]))
         # Test node_2_peers_by_ip
-        connected_node_2_ip = self.api_data.status_content_2["result"]["connected_nodes"]
-        self.assertIn(self.api_data.env_dict["NODE_1_IP"], str(connected_node_2_ip))
+        self.assertIn(self.env_dict["NODE_1_IP"], str(req_node_2_public["result"]["connected_nodes"]))
 
 
 if __name__ == "__main__":
