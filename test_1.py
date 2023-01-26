bbf5265282f3c98cc96d6653f4ddb35bfd502f51
Reda-R <mohamed-reda.ramdani@epitech.eu>
2023-01-26 17:18:27 +0100

    better use of getattr function and generic fcts for the requests

--- test_1.py
+++ test_1.py
@@ -23,10 +23,7 @@ def get_env_data(env_path: str) -> dict:
 
 
 class JsonApi():
-    #def __init__(self) -> None:
-    #    self.headers, self.payload = self.get_status()
-
-    def get_status(self):
+    def get_status(self) -> tuple[dict[str, str], str]:
         headers = {'Content-type': 'application/json'}
         payload = json.dumps({
             "jsonrpc": "2.0",
@@ -37,40 +34,48 @@ class JsonApi():
         return headers, payload
 
 
-class Api(JsonApi):
-    def __init__(self) -> None:
-        self.headers, self.payload = getattr(JsonApi, 'get_status')('self')
+class Api():
+    def __init__(self, url_public) -> None:
+        self.url_public = url_public
+        self._api = JsonApi()
 
-    def make_request(self, url_public: str):
-        response = requests.post(url_public, data=self.payload, headers=self.headers)
+    def make_request(self, url_public: str, headers, payload):
+        response = requests.post(url_public, headers=headers, data=payload)
         return response.json()
+
+    def __getattr__(self, item):
+        print("__getattribute__ ", item)
+        f = getattr(self._api, item)
+        headers, payload = f()
+        return self.make_request(self.url_public, headers, payload)
 
 
 class TestStringMethods(unittest.TestCase):
     def setUp(self) -> None:
         self.env_dict = get_env_data(".env")
-        self.api_data = Api()
+        self.api_public_1 = Api(self.env_dict["URL_PUBLIC_NODE_1"])
+        self.api_public_2 = Api(self.env_dict["URL_PUBLIC_NODE_2"])
 
     def test_status_nodes(self):
-        req_node_1_public = getattr(self.api_data, 'make_request')(self.env_dict["URL_PUBLIC_NODE_1"])
-        req_node_2_public = getattr(self.api_data, 'make_request')(self.env_dict["URL_PUBLIC_NODE_2"])
-        
+        content_get_status_1 = self.api_public_1.get_status()
+        content_get_status_2 = self.api_public_2.get_status()
+
         # Test node_1_ban_count
-        self.assertEqual(req_node_1_public["result"]["network_stats"]["banned_peer_count"], 0)
+        self.assertEqual(content_get_status_1["result"]["network_stats"]["banned_peer_count"], 0)
         # Test node_2_ban_count
-        self.assertEqual(req_node_2_public["result"]["network_stats"]["banned_peer_count"], 0)
+        self.assertEqual(content_get_status_2["result"]["network_stats"]["banned_peer_count"], 0)
         # Test node_1_ip
-        self.assertEqual(req_node_1_public["result"]["node_ip"], self.env_dict["NODE_1_IP"])
+        self.assertEqual(content_get_status_1["result"]["node_ip"], self.env_dict["NODE_1_IP"])
         # Test node_2_ip
-        self.assertEqual(req_node_2_public["result"]["node_ip"], self.env_dict["NODE_2_IP"])
+        self.assertEqual(content_get_status_2["result"]["node_ip"], self.env_dict["NODE_2_IP"])
         # Test node_1_peers_by_id
-        self.assertIn(req_node_2_public["result"]["node_id"], req_node_1_public["result"]["connected_nodes"])
+        self.assertIn(content_get_status_2["result"]["node_id"], content_get_status_1["result"]["connected_nodes"])
         # Test node_2_peers_by_id
-        self.assertIn(req_node_1_public["result"]["node_id"], req_node_2_public["result"]["connected_nodes"])
+        self.assertIn(content_get_status_1["result"]["node_id"], content_get_status_2["result"]["connected_nodes"])
         # Test node_1_peers_by_ip
-        self.assertIn(self.env_dict["NODE_2_IP"], str(req_node_1_public["result"]["connected_nodes"]))
+        self.assertIn(self.env_dict["NODE_2_IP"], str(content_get_status_1["result"]["connected_nodes"]))
         # Test node_2_peers_by_ip
-        self.assertIn(self.env_dict["NODE_1_IP"], str(req_node_2_public["result"]["connected_nodes"]))
+        self.assertIn(self.env_dict["NODE_1_IP"], str(content_get_status_2["result"]["connected_nodes"]))
 
 
 if __name__ == "__main__":
